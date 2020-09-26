8a2d518365e09df2f8dfd937f6e85e091cd931c0
lizhi-a <2507332458@qq.com>
2020-09-26 23:34:09 +0800

    section1

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,6 +1,19 @@
 'use strict';
 
-function collectSameElements(collectionA, objectB) {
-//  在这里写代码
-	
+function collectSameElements(collectionA, collectionB) {
+		//  在这里写代码
+		var array = new Array();
+        var k=0;
+        collectionB=collectionB.value;
+		for(var i=0;i<collectionA.length;i++){
+				for(var j=0;j<collectionB.length;j++){
+					if(collectionA[i]==collectionB[j]){
+						array[k]=collectionA[i];
+						k++;
+						break;
+					}
+				}
+		}
+		return array;
 }
+
